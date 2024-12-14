02227ccaf8da4635adeb12c42ebff234e1525852
Your Name <barsaranjeet15@gmail.com>
2024-12-14 22:05:18 +0530

    feat: added two numbers returns sum case

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,3 +1,6 @@
 module.exports.Add = (numbers) => {
     if (!numbers) return 0;
+    return numbers.split(',').reduce((pre, cur, cind) => {
+        return parseInt(cur) + pre
+    }, 0)
 } 
--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -3,8 +3,19 @@ const { Add } = require('./stringCalculator')
 describe('stringCalculator', () => {
     test('Check if empty string returns 0', () => {
         const input = '';
-        const  expected = 0;
-        const result = Add('');
-        expect(result).toBe(0);
-    })
+        const expected = 0;
+        const result = Add(input);
+        expect(result).toBe(expected);
+    });
+
+    test('Check if two numbers returns sum', () => {
+        [
+            { input: '1', expected: 1 },
+            { input: '1,2', expected: 3 },
+            { input: '10,10', expected: 20 }
+        ].map(cases => {
+            const result = Add(cases.input);
+            expect(result).toBe(cases.expected);
+        });
+    });
 })
